6c297d638f294318bb0dc5481cebe2fd04edcd58
KangWooJu <space1102@naver.com>
2026-03-21 19:53:24 +0900

    feat : photo 도메인 조회 DTO 추가

---
+++ src/main/java/org/woojukang/remixlab/query/creation/dto/request/ShowPhotoSelectedRequest.java
@@ -0,0 +1,7 @@
+package org.woojukang.remixlab.query.creation.dto.request;
+
+import java.util.List;
+
+public record ShowPhotoSelectedRequest(Long creationId,
+                                       Long userId) {
+}
